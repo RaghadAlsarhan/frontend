2ebf5ed4bfa88db7c9900c73bf21c717d337b823
[Ohood1Hamad] <[Ohood.Hamad.eam.js@tuwaiq.edu.sa]>
2021-12-21 11:33:13 +0300

    frontend update pages

--- pages/Products/Index.js
+++ pages/Products/Index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import Image from "next/image";
+import styles from "../../styles/Products.module.css"
+export default function Products({ data }) {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className={styles.card.prod}>
+            {data.map((elem, i) => (
+              <div key={i}>
+                <Card style={{ width: "18rem" }}>
+                  <Image
+                    src={`${elem.imge}`}
+                    alt="Products images"
+                    width={400}
+                    height={300}
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      <h1> {elem.name} </h1>
+                    </Card.Title>
+                    <Card.Text>{elem.desc}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+export async function getServerSideProps(context) {
+  const res = await fetch("http://localhost:5000/product/");
+  const data = await res.json();
 
-export default function Products() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return {
+    props: {
+      data,
+    },
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from "next/link"
+import Head from "next/head";
+// import Image from 'next/image'
+import styles from "../styles/Home.module.css";
+ import Link from "next/link"
+import {Container,Row,Col,Card,Button} from "react-bootstrap";
+
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Beauty Center</title>
         <meta name="description" content="beauty center " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Link href='/branches'> 
-Branchs
-      </Link>
+      <div className={styles.img}>
+        {/* <Image src="https://www.al3nan.com/wp-content/uploads/2017/08/102727.jpg" alt="Home Page img " width={700} height={1500} /> */}
+      </div>
+      <Row>
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Magna veniam cillum esse enim aute reprehenderit sit. Adipisicing
+          labore est proident culpa est velit excepteur reprehenderit excepteur.
+          Laborum cupidatat dolor exercitation duis eu id. Officia fugiat
+          consectetur consectetur magna culpa dolore cupidatat tempor ipsum
+          incididunt Lorem laboris adipisicing. Proident exercitation ullamco
+          nulla id aute. Lorem aute adipisicing velit occaecat deserunt
+          reprehenderit voluptate cillum eiusmod.
+        </p>
+      </div>
+      </Row>
+      <Row>
+      <div>
+      <Container>
 
+<Row >
+  <Col><Card body>MEN</Card></Col>
+  <Col><Card body>WOMEN</Card></Col>
+  <Col><Card body>KIDS</Card></Col>
+</Row>
+</Container>
+      </div>
+      </Row>
     </div>
-  )
+  );
 }
