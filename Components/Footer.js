8dd1e2dffbc24925396b027e993fd29bcff3dfd5
Amna <amnah.mohammed.eam.js@tuwaiq.edu.sa>
2021-12-23 10:06:08 +0300

    Revert "header"
    This reverts commit 2900de0b29d719f71d081199f2645a4030484082.

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,33 +1,11 @@
-// import styles from "../styles/Footer.module.css";
-import { TiSocialFacebook, TiSocialInstagram } from "react-icons/ti";
-import { AiFillTwitterSquare } from "react-icons/ai";
-import styles from "../styles/footer.module.css";
+import styles from "../styles/Footer.module.css";
+
+
 
 export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      <div>
-        <h1>Beauty Center</h1>
-      </div>
-      <div>
-        <ul>
-          
-            {" "}
-            <TiSocialFacebook />
-         
-         
-            {" "}
-            <AiFillTwitterSquare />
-          
-         
-            {" "}
-            <TiSocialInstagram />
-       
-        </ul>
-      </div>
-      <div>
-        <p>copyright © 2021</p>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            
+        </div>
+    )
 }
