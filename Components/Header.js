61d1e23f8d6e1678a67832d400158f37531a880e
Amna <amnah.mohammed.eam.js@tuwaiq.edu.sa>
2021-12-23 10:12:23 +0300

    reverting package lock

--- Components/Header.js
+++ Components/Header.js
@@ -1,37 +1,32 @@
-import styles from "../styles/Header.module.css";
-
+// import styles from "../styles/Header.module.css";
+// import logo from "../public/Img/logo.jpg";
 // import Link from "next/link";
 
-import { Nav, Container, Navbar, NavDropdown } from "react-bootstrap";
-import Image from "next/image";
+import { Nav, Container, Navbar } from "react-bootstrap";
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div>
+    <Nav>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">
-            {" "}
-            <Image src={"/Img/logo.jpg"} width={110} height={100} />
-          </Navbar.Brand>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
             <Nav>
-              <Nav.Link href="/Home">Home</Nav.Link>
-              <Nav.Link eventKey={2} href="/Products">
-                Products
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
               </Nav.Link>
-              <Nav.Link eventKey={2} href="/Register">
-                Register
-              </Nav.Link>
-              <Nav.Link eventKey={2} href="/Appointment">
-                Appointment
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </div>
+      <Image src={'/Img/logo.jpg'} width={400} height={500}/>
+    </Nav>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Leyout from "../Components/Leyout";
 import "../styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
